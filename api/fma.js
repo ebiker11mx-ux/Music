0afa3f604a941e58ba9dad7d4fcf4a06c96b2aa6
ebiker11mx-ux <ebiker11mx@gmail.com>
2025-12-29 16:02:33 +0200

    new

--- api/fma.js
+++ api/fma.js
@@ -10,4 +10,3 @@ export default async function handler(req, res) {
                                     res.status(500).json({ error: "Failed to fetch FMA results" });
                                       }
                                       }
-}
